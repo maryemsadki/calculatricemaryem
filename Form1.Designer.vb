981e071fe905e8994786490366289be5e0a6101e
Maryem <Maryem@DESKTOP-7FSHKDT>
2024-10-31 21:11:54 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.vb
@@ -0,0 +1,241 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Txt_affiche = New TextBox()
+        Btn_1 = New Button()
+        Btn_2 = New Button()
+        btn_3 = New Button()
+        Btn_4 = New Button()
+        Btn_5 = New Button()
+        Btn_6 = New Button()
+        Btn_7 = New Button()
+        Btn_8 = New Button()
+        Btn_9 = New Button()
+        Btn_effc = New Button()
+        btn_equal = New Button()
+        Btn_plus = New Button()
+        Btn_0 = New Button()
+        Btn_div = New Button()
+        Btn_multi = New Button()
+        Btn_soust = New Button()
+        SuspendLayout()
+        ' 
+        ' Txt_affiche
+        ' 
+        Txt_affiche.Location = New Point(320, 72)
+        Txt_affiche.Name = "Txt_affiche"
+        Txt_affiche.Size = New Size(125, 27)
+        Txt_affiche.TabIndex = 0
+        ' 
+        ' Btn_1
+        ' 
+        Btn_1.Location = New Point(267, 143)
+        Btn_1.Name = "Btn_1"
+        Btn_1.Size = New Size(35, 29)
+        Btn_1.TabIndex = 1
+        Btn_1.Text = "1"
+        Btn_1.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_2
+        ' 
+        Btn_2.Location = New Point(320, 143)
+        Btn_2.Name = "Btn_2"
+        Btn_2.Size = New Size(35, 29)
+        Btn_2.TabIndex = 2
+        Btn_2.Text = "2"
+        Btn_2.UseVisualStyleBackColor = True
+        ' 
+        ' btn_3
+        ' 
+        btn_3.Location = New Point(383, 143)
+        btn_3.Name = "btn_3"
+        btn_3.Size = New Size(35, 29)
+        btn_3.TabIndex = 3
+        btn_3.Text = "3"
+        btn_3.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_4
+        ' 
+        Btn_4.Location = New Point(449, 143)
+        Btn_4.Name = "Btn_4"
+        Btn_4.Size = New Size(35, 29)
+        Btn_4.TabIndex = 4
+        Btn_4.Text = "4"
+        Btn_4.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_5
+        ' 
+        Btn_5.Location = New Point(267, 191)
+        Btn_5.Name = "Btn_5"
+        Btn_5.Size = New Size(35, 29)
+        Btn_5.TabIndex = 5
+        Btn_5.Text = "5"
+        Btn_5.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_6
+        ' 
+        Btn_6.Location = New Point(320, 191)
+        Btn_6.Name = "Btn_6"
+        Btn_6.Size = New Size(35, 29)
+        Btn_6.TabIndex = 6
+        Btn_6.Text = "6"
+        Btn_6.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_7
+        ' 
+        Btn_7.Location = New Point(383, 191)
+        Btn_7.Name = "Btn_7"
+        Btn_7.Size = New Size(35, 29)
+        Btn_7.TabIndex = 7
+        Btn_7.Text = "7"
+        Btn_7.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_8
+        ' 
+        Btn_8.Location = New Point(449, 191)
+        Btn_8.Name = "Btn_8"
+        Btn_8.Size = New Size(35, 29)
+        Btn_8.TabIndex = 8
+        Btn_8.Text = "8"
+        Btn_8.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_9
+        ' 
+        Btn_9.Location = New Point(267, 251)
+        Btn_9.Name = "Btn_9"
+        Btn_9.Size = New Size(35, 29)
+        Btn_9.TabIndex = 9
+        Btn_9.Text = "9"
+        Btn_9.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_effc
+        ' 
+        Btn_effc.Location = New Point(320, 251)
+        Btn_effc.Name = "Btn_effc"
+        Btn_effc.Size = New Size(35, 29)
+        Btn_effc.TabIndex = 10
+        Btn_effc.Text = "C"
+        Btn_effc.UseVisualStyleBackColor = True
+        ' 
+        ' btn_equal
+        ' 
+        btn_equal.Location = New Point(383, 251)
+        btn_equal.Name = "btn_equal"
+        btn_equal.Size = New Size(35, 29)
+        btn_equal.TabIndex = 11
+        btn_equal.Text = "="
+        btn_equal.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_plus
+        ' 
+        Btn_plus.Location = New Point(449, 256)
+        Btn_plus.Name = "Btn_plus"
+        Btn_plus.Size = New Size(35, 29)
+        Btn_plus.TabIndex = 12
+        Btn_plus.Text = "+"
+        Btn_plus.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_0
+        ' 
+        Btn_0.Location = New Point(449, 313)
+        Btn_0.Name = "Btn_0"
+        Btn_0.Size = New Size(35, 29)
+        Btn_0.TabIndex = 13
+        Btn_0.Text = "0"
+        Btn_0.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_div
+        ' 
+        Btn_div.Location = New Point(383, 313)
+        Btn_div.Name = "Btn_div"
+        Btn_div.Size = New Size(35, 29)
+        Btn_div.TabIndex = 14
+        Btn_div.Text = "/"
+        Btn_div.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_multi
+        ' 
+        Btn_multi.Location = New Point(320, 313)
+        Btn_multi.Name = "Btn_multi"
+        Btn_multi.Size = New Size(35, 29)
+        Btn_multi.TabIndex = 15
+        Btn_multi.Text = "*"
+        Btn_multi.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_soust
+        ' 
+        Btn_soust.Location = New Point(267, 313)
+        Btn_soust.Name = "Btn_soust"
+        Btn_soust.Size = New Size(35, 29)
+        Btn_soust.TabIndex = 16
+        Btn_soust.Text = "-"
+        Btn_soust.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Btn_soust)
+        Controls.Add(Btn_multi)
+        Controls.Add(Btn_div)
+        Controls.Add(Btn_0)
+        Controls.Add(Btn_plus)
+        Controls.Add(btn_equal)
+        Controls.Add(Btn_effc)
+        Controls.Add(Btn_9)
+        Controls.Add(Btn_8)
+        Controls.Add(Btn_7)
+        Controls.Add(Btn_6)
+        Controls.Add(Btn_5)
+        Controls.Add(Btn_4)
+        Controls.Add(btn_3)
+        Controls.Add(Btn_2)
+        Controls.Add(Btn_1)
+        Controls.Add(Txt_affiche)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Txt_affiche As TextBox
+    Friend WithEvents Btn_1 As Button
+    Friend WithEvents Btn_2 As Button
+    Friend WithEvents btn_3 As Button
+    Friend WithEvents Btn_4 As Button
+    Friend WithEvents Btn_5 As Button
+    Friend WithEvents Btn_6 As Button
+    Friend WithEvents Btn_7 As Button
+    Friend WithEvents Btn_8 As Button
+    Friend WithEvents Btn_9 As Button
+    Friend WithEvents Btn_effc As Button
+    Friend WithEvents btn_equal As Button
+    Friend WithEvents Btn_plus As Button
+    Friend WithEvents Btn_0 As Button
+    Friend WithEvents Btn_div As Button
+    Friend WithEvents Btn_multi As Button
+    Friend WithEvents Btn_soust As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,165 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Security.Cryptography.Xml
+
+Public Class Form1
+    Dim A As String = ""
+    Dim B As String = ""
+    Dim op As String = ""
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+    End Sub
+
+    Private Sub Btn_0_Click(sender As Object, e As EventArgs) Handles Btn_0.Click
+        If op = "" Then
+            A = A + "0"
+        Else
+            B = B + "0"
+        End If
+        Txt_affiche.Text = A + " " + op + " " + B
+
+
+    End Sub
+
+    Private Sub Btn_1_Click(sender As Object, e As EventArgs) Handles Btn_1.Click
+        If op = "" Then
+            A = A + "1"
+        Else
+            B = B + "1"
+        End If
+        Txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Btn_2_Click(sender As Object, e As EventArgs) Handles Btn_2.Click
+        If op = "" Then
+            A = A + "2"
+        Else
+            B = B + "2"
+        End If
+        Txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub btn_3_Click(sender As Object, e As EventArgs) Handles btn_3.Click
+        If op = "" Then
+            A = A + "3"
+        Else
+            B = B + "3"
+        End If
+        Txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Btn_4_Click(sender As Object, e As EventArgs) Handles Btn_4.Click
+        If op = "" Then
+            A = A + "4"
+        Else
+            B = B + "4"
+        End If
+        Txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Btn_5_Click(sender As Object, e As EventArgs) Handles Btn_5.Click
+        If op = "" Then
+            A = A + "5"
+        Else
+            B = B + "5"
+        End If
+        Txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Btn_6_Click(sender As Object, e As EventArgs) Handles Btn_6.Click
+        If op = "" Then
+            A = A + "6"
+        Else
+            B = B + "6"
+        End If
+        Txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Btn_7_Click(sender As Object, e As EventArgs) Handles Btn_7.Click
+        If op = "" Then
+            A = A + "7"
+        Else
+            B = B + "7"
+        End If
+        Txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Btn_8_Click(sender As Object, e As EventArgs) Handles Btn_8.Click
+        If op = "" Then
+            A = A + "8"
+        Else
+            B = B + "8"
+        End If
+        Txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Btn_9_Click(sender As Object, e As EventArgs) Handles Btn_9.Click
+        If op = "" Then
+            A = A + "9"
+        Else
+            B = B + "9"
+        End If
+        Txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Btn_plus_Click(sender As Object, e As EventArgs) Handles Btn_plus.Click
+        If Not A = "" Then
+            op = "+"
+        End If
+        Txt_affiche.Text = A + " " + op + " " + B
+
+    End Sub
+
+    Private Sub Btn_soust_Click(sender As Object, e As EventArgs) Handles Btn_soust.Click
+        If Not A = "" Then
+            op = "-"
+        End If
+        Txt_affiche.Text = A + " " + op + " " + B
+
+    End Sub
+
+    Private Sub Btn_multi_Click(sender As Object, e As EventArgs) Handles Btn_multi.Click
+        If Not A = "" Then
+            op = "*"
+        End If
+        Txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Btn_div_Click(sender As Object, e As EventArgs) Handles Btn_div.Click
+        If Not A = "" Then
+            op = "/"
+        End If
+        Txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub btn_equal_Click(sender As Object, e As EventArgs) Handles btn_equal.Click
+        Dim test As Boolean = True
+        Dim res As Double
+        Dim val, value2 As Double
+        If IsNumeric(A) And IsNumeric(B) And Not op = "" Then
+            val = Integer.Parse(A)
+            value2 = Integer.Parse(B)
+            If op = "+" Then
+                res = val + value2
+            ElseIf op = "-" Then
+                res = val - value2
+            ElseIf op = "*" Then
+                res = val * value2
+            ElseIf op = "/" And Not op = "" Then
+                res = val / value2
+            Else
+                MessageBox.Show("division par 0")
+                test = False
+            End If
+        Else
+            MessageBox.Show("Aet B doivent etre des entiers")
+            test = False
+        End If
+        If test Then
+            Txt_affiche.Text = Txt_affiche.Text + "=" + res.ToString
+
+
+        End If
+    End Sub
+End Class
